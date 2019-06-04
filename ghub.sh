52866dab554fd1c4b965120886b7af79739925ce
Tocha <emerson.barea@gmail.com>
2019-06-04 14:49:09 -0300

    comment

--- ghub.sh
+++ ghub.sh
@@ -1,2 +1,6 @@
+rm -rf output
+rm -rf .idea
+rm graph/js/*
+
 git add --all && git commit -m "comment"
 git push -u origin master
--- parse.py
+++ parse.py
@@ -7,6 +7,7 @@ import time
 import os
 import shutil
 
+
 class Parse(object):
     def __init__(self, config_path):
         # get config parameters
@@ -23,14 +24,14 @@ class Parse(object):
     def df_from_file(self):
         # read CAIDA file to Pandas data frame
         self.df_from_file = pd.read_csv(self.file,
-                                   sep='|',
-                                   comment='#',
-                                   header=None,
-                                   skip_blank_lines=True,
-                                   usecols=[0, 1, 2, 3],
-                                   names=['as1', 'as2', 'pp_cp', 'got_from'],
-                                   index_col=['as1'])
-        #print(self.df_from_file)
+                                        sep='|',
+                                        comment='#',
+                                        header=None,
+                                        skip_blank_lines=True,
+                                        usecols=[0, 1, 2, 3],
+                                        names=['as1', 'as2', 'pp_cp', 'got_from'],
+                                        index_col=['as1'])
+        # print(self.df_from_file)
         #       as1         as2        pp_cp        got_from
         #        1           2          -1             bgp
         #        1           3           0             mlp
@@ -40,7 +41,7 @@ class Parse(object):
         #   AS Relationships, Customer Cones, and Validation = (got_from - bgp)
         #   AS relationships inferred from Ark traceroutes, and from multilateral peering = (got_from - mlp)
 
-    def datafames(self):
+    def data_frames(self):
         """
             populate peering dataframe (as1, as2, peer1_IP, peer2_IP, peer_mask, prefix1(prefix, mask, ...), prefix2(prefix, mask, ...))
         """
@@ -68,17 +69,17 @@ class Parse(object):
             self.df_prefix1 = pd.DataFrame(list_prefix)
             self.df_prefix1.set_index('as', inplace=True)
 
-            #print('quantidade de ASes únicos na topologia: ' + str(len(self.list_unique_as)))
+            # print('quantidade de ASes únicos na topologia: ' + str(len(self.list_unique_as)))
             # qtd de ASes únicos na topologia: 64758
 
-            #print(self.df_from_file)
+            # print(self.df_from_file)
             # as1        as2    pp_cp got_from   peer1_IP   peer2_IP  peer_mask
             # 1         3549      0      bgp     16777217   16777470         24
             # 1        11537      0      bgp     16777473   16777726         24
             # 1       133296      0      bgp     16777729   16777982         24
             # 2         6147     -1      bgp     16777985   16778238         24
 
-            #print(self.df_prefix1)
+            # print(self.df_prefix1)
             # as      mask     prefix
             # 1         24   16777216
             # 3549      24   16777216
@@ -93,26 +94,29 @@ class Parse(object):
         # elements
         self.list_create_mininet_elements_commands = list()
         print('    - elements')
-        if self.container_type == 'MININET': # ("MININET" | "DOCKER")
+        if self.container_type == 'MININET':  # ("MININET" | "DOCKER")
             for AS in self.list_unique_as:
                 self.list_create_mininet_elements_commands.append("AS%s = net.addHost('AS%s', ip=None)" % (AS, AS))
-        elif self.container_type == 'DOCKER': # ("MININET" | "DOCKER")
+        elif self.container_type == 'DOCKER':  # ("MININET" | "DOCKER")
             for AS in self.list_unique_as:
-                self.list_create_mininet_elements_commands.append("AS%s = net.addDocker('AS%s', ip=None, dimage='alpine-quagga:latest')" % (AS, AS))
+                self.list_create_mininet_elements_commands.append(
+                    "AS%s = net.addDocker('AS%s', ip=None, dimage='alpine-quagga:latest')" % (AS, AS))
 
-        #print(self.list_create_mininet_elements_commands)
-        #AS25970 = net.addHost('AS25970', ip=None)
-        #AS265702 = net.addHost('AS265702', ip=None)
+        # print(self.list_create_mininet_elements_commands)
+        # AS25970 = net.addHost('AS25970', ip=None)
+        # AS265702 = net.addHost('AS265702', ip=None)
 
         # links
         print('    - links')
         self.list_create_mininet_links_commands = list()
         for row in self.df_from_file.itertuples():
-            self.list_create_mininet_links_commands.append("net.addLink(AS%s, AS%s, intfName1='%s-%s', intfName2 = '%s-%s', params1={'ip':'%s/24'}, params2={'ip':'%s/24'})" % \
-                                                           (row[0], row[1], row[0], row[1], row[1], row[0], str(ipaddress.ip_address(row[4])), str(ipaddress.ip_address(row[5]))))
-        #print(self.list_create_mininet_links_commands)
-        #net.addLink(AS397095, AS30168, intfName1='397095-30168', intfName2='30168-397095', params1={'ip': '9.44.173.1/24'}, params2={'ip': '9.44.173.254/24'})
-        #net.addLink(AS397095, AS63343, intfName1='397095-63343', intfName2='63343-397095', params1={'ip': '9.44.174.1/24'}, params2={'ip': '9.44.174.254/24'})
+            self.list_create_mininet_links_commands.append(
+                "net.addLink(AS%s, AS%s, intfName1='%s-%s', intfName2 = '%s-%s', params1={'ip':'%s/24'}, params2={'ip':'%s/24'})" % \
+                (row[0], row[1], row[0], row[1], row[1], row[0], str(ipaddress.ip_address(row[4])),
+                 str(ipaddress.ip_address(row[5]))))
+        # print(self.list_create_mininet_links_commands)
+        # net.addLink(AS397095, AS30168, intfName1='397095-30168', intfName2='30168-397095', params1={'ip': '9.44.173.1/24'}, params2={'ip': '9.44.173.254/24'})
+        # net.addLink(AS397095, AS63343, intfName1='397095-63343', intfName2='63343-397095', params1={'ip': '9.44.174.1/24'}, params2={'ip': '9.44.174.254/24'})
 
     def quagga_commands(self):
         """
@@ -126,17 +130,23 @@ class Parse(object):
         list_routerid = list()
         for row in self.df_from_file.itertuples():
             # zebra
-            list_create_zebra_interfaces.append({'as': row[0], 'command': 'interface %s-%s\n  ip address %s/%s\n\n' % (row[0], row[1], str(ipaddress.ip_address(row[4])), str(row[6]))})
-            list_create_zebra_interfaces.append({'as': row[1], 'command': 'interface %s-%s\n  ip address %s/%s\n\n' % (row[1], row[0], str(ipaddress.ip_address(row[5])), str(row[6]))})
+            list_create_zebra_interfaces.append({'as': row[0], 'command': 'interface %s-%s\n  ip address %s/%s\n\n' % (
+            row[0], row[1], str(ipaddress.ip_address(row[4])), str(row[6]))})
+            list_create_zebra_interfaces.append({'as': row[1], 'command': 'interface %s-%s\n  ip address %s/%s\n\n' % (
+            row[1], row[0], str(ipaddress.ip_address(row[5])), str(row[6]))})
             # bgpd - router ID
             list_routerid.append({'as': row[0], 'router_ID': row[4]})
             list_routerid.append({'as': row[1], 'router_ID': row[5]})
             # bgpd - neighbor
-            list_create_bgpd_neighbor.append({'as': row[0], 'command': '  neighbor %s remote-as %s\n' % (str(ipaddress.ip_address(row[5])), row[1])})
-            list_create_bgpd_neighbor.append({'as': row[1], 'command': '  neighbor %s remote-as %s\n' % (str(ipaddress.ip_address(row[4])), row[0])})
+            list_create_bgpd_neighbor.append(
+                {'as': row[0], 'command': '  neighbor %s remote-as %s\n' % (str(ipaddress.ip_address(row[5])), row[1])})
+            list_create_bgpd_neighbor.append(
+                {'as': row[1], 'command': '  neighbor %s remote-as %s\n' % (str(ipaddress.ip_address(row[4])), row[0])})
             # bgpd - network (prefix)
-            list_create_bgpd_prefix.append({'as': row[0], 'command': '  network %s\n' % (str(ipaddress.ip_network(str(ipaddress.ip_address(row[4])) + '/24', strict=False)))})
-            list_create_bgpd_prefix.append({'as': row[1], 'command': '  network %s\n' % (str(ipaddress.ip_network(str(ipaddress.ip_address(row[5])) + '/24', strict=False)))})
+            list_create_bgpd_prefix.append({'as': row[0], 'command': '  network %s\n' % (
+                str(ipaddress.ip_network(str(ipaddress.ip_address(row[4])) + '/24', strict=False)))})
+            list_create_bgpd_prefix.append({'as': row[1], 'command': '  network %s\n' % (
+                str(ipaddress.ip_network(str(ipaddress.ip_address(row[5])) + '/24', strict=False)))})
         self.df_create_zebra_interfaces = pd.DataFrame(list_create_zebra_interfaces)
         self.df_create_zebra_interfaces.set_index('as', inplace=True)
         self.df_create_bgpd_neighbor = pd.DataFrame(list_create_bgpd_neighbor)
@@ -147,35 +157,35 @@ class Parse(object):
         self.df_create_routerid_temp = pd.DataFrame(list_routerid)
         self.df_create_routerid_temp.set_index('as', inplace=True)
         self.df_create_routerid = pd.DataFrame(
-        self.df_create_routerid_temp.reset_index()[['as', 'router_ID']].groupby(by='as').min(axis=1))
+            self.df_create_routerid_temp.reset_index()[['as', 'router_ID']].groupby(by='as').min(axis=1))
 
         self.df_create_routerid = self.df_create_routerid.astype({'router_ID': str})
         for i, row in self.df_create_routerid.iterrows():
             self.df_create_routerid.at[i, 'router_ID'] = '  router id %s\n' % (str(ipaddress.ip_address(int(row[0]))))
 
-        #print(self.df_from_file.reset_index()[['as1', 'peer1_IP']].groupby(by='as1').min(axis=1))
+        # print(self.df_from_file.reset_index()[['as1', 'peer1_IP']].groupby(by='as1').min(axis=1))
 
-        #print(self.df_create_zebra_commands)
+        # print(self.df_create_zebra_commands)
         # as            command
         # 1             interface 1-3549\n  ip address 1.0.0.1/24
         # 3549          interface 3549-1\n  ip address 1.0.0.254/24
 
-        #print(self.df_create_bgpd_neighbor_commands)
+        # print(self.df_create_bgpd_neighbor_commands)
         # as		command
         # 397285    	neighbor 9.44.178.254 remote-as 397268
         # 397268      	neighbor 9.44.178.1 remote-as 397285
 
-        #print(self.df_create_bgpd_prefix_commands)
+        # print(self.df_create_bgpd_prefix_commands)
         # as	   command
         # 397187    network 9.44.177.0/24
         # 45974     network 9.44.177.0/24
         # 397285    network 9.44.178.0/24
         # 397268    network 9.44.178.0/24
 
-        #print(self.df_create_routerid)
-        #as         router_ID
-        #397437     router id 4.122.238.254
-        #397444     router id 3.135.39.254
+        # print(self.df_create_routerid)
+        # as         router_ID
+        # 397437     router id 4.122.238.254
+        # 397444     router id 3.135.39.254
 
     def write_to_file(self):
         """
@@ -271,7 +281,8 @@ class Parse(object):
         with open(self.graph_dir + 'edges.js', 'w') as write_to_file:
             write_to_file.write('var edges = new vis.DataSet([\n')
             for row in self.df_from_file.itertuples():
-                write_to_file.write('{from: %s, to: %s},\n' % (row[0], row[1]))
+                color = 'red' if row[2] == 0 else 'black'
+                write_to_file.write('{from: %s, to: %s, color:{color:"%s"}},\n' % (row[0], row[1], color))
             write_to_file.write(']);')
         write_to_file.close()
 
@@ -284,7 +295,7 @@ if __name__ == '__main__':
     parse.df_from_file()
     t1 = time.time()
     print(t1 - start)
-    parse.datafames()
+    parse.data_frames()
     t2 = time.time()
     print(t2 - t1)
     parse.mininet_commands()
